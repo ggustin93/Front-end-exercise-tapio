c2ef35741b8b6cb06108a7ab3ceb2fce69277fe0
guillaume.gustin.9@gmail.com <guillaume.gustin.9@gmail.com>
2023-02-16 00:18:47 +0100

    Added comments

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Post from './Post'
 
 const PostList = ({ posts, deletePost }) => {
+  // Check if the posts array is empty and return null if it is
   if (posts.length === 0) {
     return null
   }
   return (
+    // Set up a grid layout with 2 columns on large screens
     <div className="grid gap-8 lg:grid-cols-2">
+      {/* For each post in the posts array, render a Post component with that post's data */}
       {posts.map((post) => (
         <Post key={post.id} data={post} deletePost={deletePost} />
       ))}
