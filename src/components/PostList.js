6baddce844099d1ef47a74ec5949f72c2164ac64
guillaume.gustin.9@gmail.com <guillaume.gustin.9@gmail.com>
2023-02-16 00:17:05 +0100

    Simplified PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,21 +2,15 @@ import React from 'react'
 import Post from './Post'
 
 const PostList = ({ posts, deletePost }) => {
-  // function to map over the posts and render the Post component for each post
-  const renderPosts = () => {
-    // return null if the posts array is empty
-    if (posts.length === 0) {
-      return null
-    }
-    // map over the posts array and render the Post component for each post
-    return posts.map((post) => (
-      <Post key={post.id} data={post} deletePost={deletePost} />
-    ))
+  if (posts.length === 0) {
+    return null
   }
-
   return (
-    // use CSS Grid to display the posts in a grid with two columns
-    <div className="grid gap-8 lg:grid-cols-2">{renderPosts()}</div>
+    <div className="grid gap-8 lg:grid-cols-2">
+      {posts.map((post) => (
+        <Post key={post.id} data={post} deletePost={deletePost} />
+      ))}
+    </div>
   )
 }
 
